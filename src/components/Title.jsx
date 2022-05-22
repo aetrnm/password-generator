f7e69c7ddbac22165e5cd7151b682314c661333d
Oleg Gnat <oleggnatkovskii@gmail.com>
2022-05-22 22:46:27 +0200

    feat: responsiveness

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 
 import Typography from "@mui/material/Typography";
+import { createTheme, responsiveFontSizes, ThemeProvider } from "@mui/material";
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme, { factor: 3 });
 
 function Title() {
   return (
-    <div style={{ marginTop: 60, marginBottom: 60 }}>
-      <Typography variant="h2" component="div" gutterBottom>
-        Random Password Generator
-      </Typography>
-      <Typography variant="h5" gutterBottom component="div">
-        Create strong and secure passwords to keep your account safe online.
-      </Typography>
+    <div style={{ marginTop: 20, marginBottom: 60 }}>
+      <ThemeProvider theme={theme}>
+        <Typography variant="h1" component="div" gutterBottom>
+          Random Password Generator
+        </Typography>
+        <Typography variant="h5" gutterBottom component="div">
+          Create strong and secure passwords to keep your account safe online.
+        </Typography>
+      </ThemeProvider>
     </div>
   );
 }
