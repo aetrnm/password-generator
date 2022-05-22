da125b350400366a5f6236cc05a76ee85ecb1638
Oleg Gnat <oleggnatkovskii@gmail.com>
2022-05-22 23:49:05 +0200

    feat: add mediaquery

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 function Icon({ src }) {
+  const matches = useMediaQuery("(min-width:1200px)");
+  const size = matches ? 300 : 200;
   return (
     <div>
-      <img src={src} alt="SVG logo" style={{ width: 300, height: 300 }} />
+      <img src={src} alt="SVG logo" height={size} width={size} />
     </div>
   );
 }
